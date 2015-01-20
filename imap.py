4ad1abd51fd09b40dd04a423cd9191e69933aabb
Evgeniy Vasilev <dev@johnynsk.ru>
2015-01-20 13:58:36 +0600

    Добавил: отключение цветов, игнорирование директорий, инлайн-режим

--- imap.py
+++ imap.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf8 -*-
 """
 Module displaying the number of unread messages
@@ -7,6 +8,7 @@ on an IMAP inbox (configurable).
 """
 
 import imaplib
+import sys
 from time import time
 
 
@@ -19,6 +21,8 @@ class Py3status:
     password = 'YOUR_PASS'
     port = '143'
     user = 'YOUR_LOGIN'
+    ignorebox = ['IGNORING_FOLDERS_HERE']
+    usecolor = 0
 
     def check_mail(self, i3s_output_list, i3s_config):
         mail_count = self._get_mail_count()
@@ -27,13 +31,15 @@ class Py3status:
             'full_text': '{}: {}'.format(self.name, mail_count)
         }
 
-        new_mail_color = i3s_config['color_good']
-        check_failed_color = i3s_config['color_bad']
+        if self.usecolor:
+            new_mail_color = i3s_config['color_good']
+            check_failed_color = i3s_config['color_bad']
 
-        if mail_count == 'N/A':
-            response['color'] = check_failed_color
-        elif mail_count != '0':
-            response['color'] = new_mail_color
+            if mail_count == 'N/A':
+                response['color'] = check_failed_color
+            elif mail_count != '0':
+                response['color'] = new_mail_color
+
         return response
 
     def _get_mail_count(self):
@@ -43,6 +49,8 @@ class Py3status:
             imap.login(self.user, self.password)
             folders = [folder.split(' "/" ')[1] for folder in imap.list()[1]]
             for box in folders:
+                if box in self.ignorebox:
+                    continue
                 typ, data = imap.select(box)
                 typ, data = imap.search(None, 'UNSEEN')
                 unseen_messages += len(data[0].split())
@@ -58,6 +66,9 @@ if __name__ == "__main__":
     """
     from time import sleep
     x = Py3status()
+    if "--inline" in sys.argv:
+        print(x.check_mail([], {}))
+        sys.exit()
     while True:
         print(x.check_mail([], {}))
         sleep(1)
